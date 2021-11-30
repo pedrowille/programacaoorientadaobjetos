d6a6f57126dccbeacced11adc253c0bf32673f01
pedrowille <pedrowille9@gmail.com>
2021-11-30 16:00:21 -0300

    Atualizado exercício orientação a objetos

--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ Pessoa objetoPessoa = new ();
         objetoPessoa.Nome = "meunome";
         objetoPessoa.Sobrenome = "meu sobrenome";
         Console.WriteLine($"O nome da pessoa é: {objetoPessoa.Nome} {objetoPessoa.Sobrenome}");
+        Pessoa novoObjetoPessoa = new("Maria", "Silva", DateTime.Now);
+        Console.WriteLine($"O nome da nova pessoa é: {novoObjetoPessoa.Nome} {novoObjetoPessoa.Sobrenome}, Data de nascimento: {novoObjetoPessoa.DataNascimento}");
         }
     }
 }
