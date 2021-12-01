13083140d6eed7ea644e6e23cd923eec1ea1e818
pedrowille <pedrowille9@gmail.com>
2021-12-01 11:51:29 -0300

    Atualizado exemplo orientação a objetos

--- Program.cs
+++ Program.cs
@@ -11,7 +11,12 @@ namespace ProgramacaoOrientadaObjetos
             objetoPessoa.Nome = "meunome";
             objetoPessoa.Sobrenome = "meu sobrenome";
             Console.WriteLine($"O nome da pessoa é: {objetoPessoa.Nome} {objetoPessoa.Sobrenome}");
-            Pessoa novoObjetoPessoa = new("Pedro", "Wille", new DateTime(1998, 04, 11));
+            Console.WriteLine("Digite a sua data de nascimento: (dd/MM/AAAA)");
+            string? textoDataNascimento = Console.ReadLine();
+            int ano = Convert.ToInt32(textoDataNascimento?.Substring(6, 4));
+            int mes = Convert.ToInt32(textoDataNascimento?.Substring(3, 2));
+            int dia = Convert.ToInt32(textoDataNascimento?.Substring(0, 2));
+            Pessoa novoObjetoPessoa = new("Pedro", "Wille", new DateTime(ano, mes, dia));
             Console.WriteLine($"O nome da nova pessoa é: {novoObjetoPessoa.Nome} {novoObjetoPessoa.Sobrenome}. {novoObjetoPessoa.CalculaIdade()}");
         }
     }
